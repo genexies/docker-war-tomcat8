658e96c3bbaba58a2ab92d84ec682341914c3e8c
Javier Jerónimo <jcjeronimo@genexies.net>
2015-04-30 19:28:08 +0200

    Fix for HTTPS

--- pkgp-run.sh
+++ pkgp-run.sh
@@ -4,7 +4,7 @@
 rm -rf ${CATALINA_HOME}/webapps/*
 
 
-curl --progress-bar ${ARTIFACT_URL} -o ${CATALINA_HOME}/webapps/${DESIRED_WEBAPP_PATH_IN_TOMCAT}.war
+curl --insecure --progress-bar ${ARTIFACT_URL} -o ${CATALINA_HOME}/webapps/${DESIRED_WEBAPP_PATH_IN_TOMCAT}.war
 
 
 exec ${CATALINA_HOME}/bin/catalina.sh run
